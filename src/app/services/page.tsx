8bab054393840028e69699decaa7860d0b40a9df
Kyaw Min Aung <kyawminaung343@gmail.com>
2026-02-06 18:00:21 +0630

    feat: implement responsive footer

--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 
 export default function page() {
   return (
-    <div className='h-screen pt-20 bg-black'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo repellat praesentium dolores possimus incidunt modi repudiandae a, et eaque maiores asperiores. Totam ipsum beatae esse eum, enim ullam fugit quae?</div>
+    <div className='h-screen pt-20 bg-black'>
+      <div className="cursor-pointer">hay</div>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo repellat praesentium dolores possimus incidunt modi repudiandae a, et eaque maiores asperiores. Totam ipsum beatae esse eum, enim ullam fugit quae?</div>
   )
 }
